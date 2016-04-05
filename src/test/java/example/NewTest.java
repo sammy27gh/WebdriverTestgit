622e17840293f0c71e475d8a9646fff36ffbfb85
sandoh <andoh.samuel@gmail.com>
2016-04-05 02:56:00 -0500

    only failed test

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,6 +1,6 @@
 package example;        
  
-import org.openqa.selenium.By;      
+     
 import org.openqa.selenium.WebDriver;       
 import org.openqa.selenium.firefox.FirefoxDriver;       
 import org.testng.Assert;       
@@ -10,13 +10,7 @@ import org.testng.annotations.AfterTest;
 public class NewTest {      
 	
         private WebDriver driver;       
-        @Test              
-        public void testEasy() {    
-            driver.get("http://www.guru99.com/selenium-tutorial.html");  
-            String title = driver.getTitle();   
-            System.out.println("Test if the title containf free selenium tutorial ");
-            Assert.assertTrue(title.contains("Free Selenium Tutorials"));       
-        }   
+        
         @Test
         public void tryanother() {    
         	System.out.println("This is the second test ");
